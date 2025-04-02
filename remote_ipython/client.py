280869d18b61d6bcd8530df0bb7f1bcdb8541631
Likianta <likianta@foxmail.com>
2025-04-02 10:31:35 +0800

    simplify server implementation

--- remote_ipython/client.py
+++ remote_ipython/client.py
@@ -5,21 +5,14 @@ reference:
     https://github.com/albertz/background-zmq-ipython/blob/master/kernel.py
 """
 
-import lk_logger
 from jupyter_console.app import ZMQTerminalIPythonApp
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
 
 def run_client(kernel_id: str) -> None:
-    lk_logger.unload()
-    
     instance = ZMQTerminalIPythonApp.instance()
-    instance.initialize(
-        argv=['console', '--existing', kernel_id],
-    )
+    instance.initialize(argv=['console', '--existing', kernel_id])
     # fix no prompt
     # https://blog.csdn.net/Likianta/article/details/131486249
     instance.shell.pt_cli.auto_suggest = AutoSuggestFromHistory()
     instance.start()  # blocking
-    
-    lk_logger.setup(quiet=True)
--- remote_ipython/server.py
+++ remote_ipython/server.py
@@ -1,101 +1,49 @@
-"""
-reference:
-    https://github.com/likianta/brilliant-ui/blob/master/brilliant/application -
-        /debug.py
-    https://github.com/albertz/background-zmq-ipython/blob/master/kernel.py
-"""
-
-if 1:
-    import os
-    import typing as t
-    from contextlib import contextmanager
-    from os import getpid
-    
-    import lk_logger
-    from lk_utils import new_thread
-
-if 2:  # import ipykernal related stuff
-    # ignore some warning from ipython kernel.
-    os.environ['PYDEVD_DISABLE_FILE_VALIDATION'] = '1'
-    
-    
-    @contextmanager
-    def _suppress_kernel_warning() -> None:
-        lk_logger.mute()  # TEST: comment this line to see what info is buried.
-        yield
-        lk_logger.unmute()
-    
-    
-    with _suppress_kernel_warning():
-        # from IPython import embed_kernel
-        from IPython.core.autocall import ZMQExitAutocall
-        from ipykernel.embed import embed_kernel
-        from ipykernel.kernelapp import IPKernelApp
-        from ipykernel.zmqshell import ZMQInteractiveShell
-
-
-class KernelWrapper:
-    _callback: t.Optional[t.Callable]
-    _pid: str
-    
-    def __init__(self, callback: t.Callable = None) -> None:
-        self._callback = callback
-    
-    @property
-    def app(self) -> IPKernelApp:
-        return IPKernelApp.instance()
-    
-    @property
-    def kernel_id(self) -> str:
-        return self._pid
-    
-    def start(self, user_ns: dict) -> None:
-        self._pid = str(getpid())
-        print(
-            '\nto connect to this kernel, use:\n'
-            '    [u][cyan]python -m [yellow]remote_ipython[/] connect-debugger '
-            '[cyan]{}[/][/][/]'.format(self._pid),
-            ':rs1',
-        )
-        
-        with _suppress_kernel_warning():
-            """
-            suppress warning from `IPKernelApp.init_signal`, because we do \
-            want to do this in a subthread.
-            """
-            embed_kernel(local_ns=user_ns)
-        
-        # after
-        print(':r', '[red dim]kernel exited[/]')
-        if self._callback:
-            self._callback()
-    
-    @new_thread()
-    def start_new_thread(self, user_ns: dict) -> None:
-        self.start(user_ns)
-    
-    def close(self) -> None:
-        self.app.close()
+import IPython
+import jupyter_console
+import lk_logger
+import os
+import typing as t
+from ipykernel.kernelapp import IPKernelApp
+from textwrap import dedent
+from threading import Thread
 
 
 def run_server(
-    user_ns: dict = None, callback: t.Callable = None, new_thread: bool = False
-) -> KernelWrapper:
-    def _leave() -> None:
-        sh = ZMQInteractiveShell.instance()
-        exiter = ZMQExitAutocall(ip=sh)
-        exiter(keep_kernel=True)
-    
-    kernel = KernelWrapper(callback=callback)
-    user_ns = {
-        'leave' : _leave,
-        'print' : lk_logger.bprint,
-        **(user_ns or {}),
-    }
-    if new_thread:
-        kernel.start_new_thread(user_ns)
-        from time import sleep
-        sleep(2)
+    user_ns: dict = None, subthread: bool = False
+) -> t.Optional[Thread]:
+    if subthread:
+        th = Thread(target=_run, args=(user_ns or {},), daemon=True)
+        th.start()
+        return th
     else:
-        kernel.start(user_ns)
-    return kernel
+        _run(user_ns or {})
+
+
+def _run(user_ns: dict) -> None:
+    """
+    https://stackoverflow.com/questions/11019440/
+    """
+    lk_logger.setup(quiet=True, show_source=False)
+    lk_logger.deflector.add(IPython, lk_logger.bprint, scope=True)
+    lk_logger.deflector.add(jupyter_console, lk_logger.bprint, scope=True)
+    
+    pid = os.getpid()
+    print(':r', dedent(
+        '''
+        - To connect to this kernel app, use:
+            [u]python -m remote_ipython run-client [yellow]{}[/][/]
+        - To disconnect client from server (which keeps server alive), press
+            [blue]Ctrl+D[/] in client side.
+        - To exit, you will have to explicitly kill this process, or send
+            [magenta]quit[/] or [magenta]exit[/] from client side.
+        '''.format(pid)
+    ))
+    
+    app = IPKernelApp.instance()
+    app.initialize()
+    app.user_ns = user_ns or {}
+    app.start()
+
+
+if __name__ == '__main__':
+    run_server({'a': 'alpha', 'b': 'beta'})
